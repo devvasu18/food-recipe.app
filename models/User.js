f71f62dca11b48d1f1ef36e1f64458086c784692
vasu <vasudevsharma9413.com>
2025-07-18 19:06:00 +0530

    fixing some minor bugs

--- models/User.js
+++ models/User.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },           
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }],
 
 });
 
