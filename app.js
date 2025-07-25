6f4e877e641c75c359cf47b3af724847d3fbd7ef
vasu <vasudevsharma9413.com>
2025-07-25 11:52:33 +0530

    deploying

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.use('/', recipeRoutes);
 
 
 // ✅ Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
